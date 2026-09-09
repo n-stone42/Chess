18fe5a5a3b2e77ae2bde3cd25619ff51dac721d8
n-stone42 <nstone42@byu.edu>
2026-09-08 22:16:40 -0600

    Logic Bug in Bishop move function but output is returning moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -53,6 +54,93 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return List.of(); //TODO
+        // takes in the board and the position of the piece
+        // returns the moves
+
+        ChessPiece the_piece = board.getPiece(myPosition);
+        if (the_piece.getPieceType() == PieceType.BISHOP){
+//            return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null));
+            Collection<ChessMove> List_1 = move_itter(board, myPosition, -1,-1);
+            Collection<ChessMove> List_2 = move_itter(board, myPosition, 1,-1);
+            Collection<ChessMove> List_3 = move_itter(board, myPosition, -1,1);
+            Collection<ChessMove> List_4 = move_itter(board, myPosition, 1,1);
+            List_1.addAll(List_2);
+            List_1.addAll(List_3);
+            List_1.addAll(List_4);
+            return List_1;
+        }
+
+        else if (the_piece.getPieceType() == PieceType.KING){
+
+        }
+
+        else if (the_piece.getPieceType() == PieceType.KNIGHT) {
+            //do knight stuff
+        }
+
+        else if (the_piece.getPieceType() == PieceType.PAWN) {
+            // do pawn stuff
+        }
+
+        else if (the_piece.getPieceType() == PieceType.QUEEN) {
+            // do queen stuff
+        }
+
+        else if (the_piece.getPieceType() == PieceType.ROOK) {
+            // do rook stuff
+        }
+
+        else {
+            return null;} // there was
+        return null;
+    }
+
+    private boolean is_empty(ChessBoard board, ChessPosition myPosition) {
+        if (board.getPiece(myPosition) == null) {
+            System.out.println("returning is empty");
+            return true;
+        }
+        else {
+            System.out.println("returning is NOT empty");
+            return false;
+        }
+    }
+
+    private boolean in_bounds(int row, int col) {
+        if ( 0 < row && row < 8 && 0 < col && col < 8){
+            System.out.printf("row %d col %d is in bounds   ", row, col);
+            return true;
+        }
+        else {
+            System.out.printf("row %d col %d is in OUT of bounds   ", row, col);
+            return false;
+        }
+    }
+
+    private boolean can_take(ChessBoard board, ChessPosition myPosition, ChessPosition newPosition) {
+
+        if (board.getPiece(myPosition).getPieceType() == board.getPiece(newPosition).getPieceType()) {
+            return false; //can't take, same color
+        }
+        return true; //different colors, can take
+    }
+    private List<ChessMove> move_itter(ChessBoard board, ChessPosition myPosition, int row_scalar, int col_scalar) {
+
+        List<ChessMove> moves = new ArrayList<>();
+        int new_row = myPosition.getRow() + row_scalar;
+        int new_col = myPosition.getColumn() + col_scalar;
+        while (in_bounds(new_row, new_col)) {
+            System.out.printf("Finding new move. Row %d col %d  ",new_row, new_col);
+            ChessPosition new_pos = new ChessPosition (new_row, new_col);
+
+            if (is_empty(board, new_pos) || can_take(board, myPosition, new_pos)) {
+
+                ChessMove new_move = new ChessMove(myPosition, new_pos, null);
+                 moves.add(new_move);
+            }
+            new_row = new_row + row_scalar;
+            new_col = new_col + row_scalar;
+        }
+        return moves;
     }
 }
